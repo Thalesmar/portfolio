be64a3834b5152cbb5fda6bbebe2bf9ce39369b7
Thalesmar <beneliot222@gmail.com>
2025-08-14 21:21:58 +0100

    1.2

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <browserRouter basename="/portfolio-react/">
     <App />
+    </browserRouter>
   </StrictMode>,
-)
+);
